a0c5a7fce3fe071476a1a0a9e446a7715c8be9b8
Carlos Astengo Macias <pg23carlos@vfs.com>
2023-01-29 18:26:20 -0800

    A1

---
+++ src/components/GraphComponent.vue
@@ -0,0 +1,49 @@
+<script>
+
+    export default {
+        name: `Graph`,
+        vm: 
+        {
+            vmText: "This will later have a graph",
+        },
+        props: 
+        {
+            title: String,
+        },
+        methods: 
+        {
+            doIt()
+            {
+                alert("did something")
+            }
+        }
+    }
+
+</script>
+
+<template>
+    <div class="borderedSquare">
+        <h1 class="title">{{ title }}</h1>
+        <h3> <slot> A Graph </slot> </h3>
+        <button @click="doIt">Do Something</button>
+    </div>
+</template>
+
+<style scoped>
+    .borderedSquare
+    {
+        height: 300px;
+        width: 500px;
+        border-color: black;
+        border-style: solid;
+        background-color: #334d6b;
+    }
+
+    .title
+    {
+        color: black;
+        padding-bottom: 15px;
+    }
+
+
+</style>
---
+++ src/routes/GraphsView.vue
@@ -0,0 +1,34 @@
+<script>
+
+import Graph from '../components/GraphComponent.vue'
+
+export default {
+    name: 'GraphView',
+    components: {
+        Graph,
+    },
+}
+
+</script>
+
+<template>
+    <div class="graphCell">
+        <Graph title="Bar Graph"><div>A bar graph</div></Graph>
+        <Graph title="Line Graph"><div>A line graph</div></Graph>
+    </div>
+    <div class="graphCell">
+        <Graph title="Pie Chart"><div> pie chart</div></Graph>
+        <Graph title="Heat Map"><div>A heat chart</div></Graph>
+    </div>
+
+</template>
+
+<style>
+    .graphCell
+    {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-evenly;
+        margin: 20px;
+    }
+</style>
